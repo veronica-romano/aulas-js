160ac8a8bc48e2ff376c9029ed97034dff62591c
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-26 10:26:01 -0300

    inserted examples
    in code

---
+++ 07-funcoes/funcoes.js
@@ -0,0 +1,9 @@
+/* Sintaxe 1 (tradicional) Função literal/anônima */
+let exemplo1 = function(){
+    console.log("Função anônima/literal");
+} 
+
+/* Sintaxe 2 (tradicional) Função nomeada */
+
+
+/* Sintaxe 3 (moderna) Função seta/flecha */
