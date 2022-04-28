cdc651d6c71f9d12dfb554ee17ea7b3f7df2a3e2
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-28 09:05:56 -0300

    Update eventos.js

--- 08-eventos/eventos.js
+++ 08-eventos/eventos.js
@@ -1,12 +1,16 @@
+/*
 for(var i = 0; i < 100; i++){
     console.log(i);
 }
-document.write(i)
+document.write(i) 
 
 function clique(){
     let nome = document.getElementById('nome').value;
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = nome;
 }
+*/
+
+
 
 
