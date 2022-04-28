87080fe8b4e85b9fa7560a1cb50f0d69a4274b1e
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-28 08:43:49 -0300

    Update script.js

--- exer1-conversor/script.js
+++ exer1-conversor/script.js
@@ -1,10 +1,7 @@
 let number;
-
-
 function formateValue(){
     let dolar = 5;
     number = parseFloat(document.getElementById("number").value);			
-
     document.getElementById("resultado").innerHTML = (number * dolar).toFixed(2);
 }
 
