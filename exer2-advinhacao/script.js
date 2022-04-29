a96f3eee89fbe08e1406b6bafc62be0d5dceb21f
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-29 12:04:14 -0300

    adding style

--- exer2-advinhacao/script.js
+++ exer2-advinhacao/script.js
@@ -6,9 +6,13 @@ formulario.addEventListener('submit', function(event){
     let palpiteUsuario = document.getElementById('palpite').value;
     let resultado = document.getElementById('resultado')
     if (palpiteUsuario == numeroSorteado){
-        resultado.innerHTML = "você acertou!";
+        resultado.innerHTML = "VOCÊ ACERTOU!";
     } else if (palpiteUsuario <0 || palpiteUsuario >10){
         resultado.innerHTML = "digite um numero entre 0 e 10";
+    } else if (palpiteUsuario < numeroSorteado){
+        resultado.innerHTML = "Escolha um número maior...";
+    }else if (palpiteUsuario > numeroSorteado) {
+        resultado.innerHTML = "escolha um número menor...";
     } else{
         resultado.innerHTML = "você errou!";
     }
