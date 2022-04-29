f71806acf0e00cd26cf47c760d7799019632e6f8
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-29 09:27:19 -0300

    convert
    adding fields

--- exer1-conversor/script.js
+++ exer1-conversor/script.js
@@ -1,8 +1,18 @@
-
+/*
 function converteDolar(){
     
     let valorDolar = document.getElementById('valorDolar').value;
     let converteReal = valorDolar * 4.97;
+    let resultado = document.getElementById('resultado')
+    resultado.innerText = `O valor convertido em reais é R$ ${converteReal}`
+    resultado.style.color = 'red';
+} */
+
+
+function converteDolar(){
+    let cotacaoDolar = document.getElementById('cotacaoDolar').value;
+    let valorDolar = document.getElementById('valorDolar').value;
+    let converteReal = valorDolar * cotacaoDolar;
     let resultado = document.getElementById('resultado')
     resultado.innerText = `O valor convertido em reais é R$ ${converteReal}`
     resultado.style.color = 'red';
