a044a0ebe4cb8b0b3ad8519a32b0f76616ae5000
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-29 09:09:02 -0300

    conversor
    js added

--- exer1-conversor/script.js
+++ exer1-conversor/script.js
@@ -3,6 +3,7 @@ function converteDolar(){
     
     let valorDolar = document.getElementById('valorDolar').value;
     let converteReal = valorDolar * 4.97;
-    
+    let resultado = document.getElementById('resultado')
+    resultado.innerText = `O valor convertido em reais é R$ ${converteReal}`
 }
 
