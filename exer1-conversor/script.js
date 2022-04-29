ada30dc71afe1bda06ec5fa215d490873e7ca8de
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-04-29 09:41:12 -0300

    soluções comentadas

--- exer1-conversor/script.js
+++ exer1-conversor/script.js
@@ -1,4 +1,4 @@
-/*
+/* soluçaõ 1
 function converteDolar(){
     
     let valorDolar = document.getElementById('valorDolar').value;
@@ -8,6 +8,16 @@ function converteDolar(){
     resultado.style.color = 'red';
 } */
 
+
+/* Solução 2
+function converteDolar(){
+    let cotacaoDolar = document.getElementById('cotacaoDolar').value;
+    let valorDolar = document.getElementById('valorDolar').value;
+    let converteReal = valorDolar * cotacaoDolar;
+    let resultado = document.getElementById('resultado')
+    resultado.innerText = `O valor convertido em reais é R$ ${converteReal}`
+    resultado.style.color = 'red';
+} */
 
 function converteDolar(){
     let cotacaoDolar = document.getElementById('cotacaoDolar').value;
